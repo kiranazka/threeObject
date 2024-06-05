99ad08bcdd569d9d341280644cd148d421f3a95f
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-06-05 22:27:36 +0500

    Three object

---
+++ index.js
@@ -0,0 +1,8 @@
+var myObject = {
+    item1: "Apple",
+    item2: "Orange",
+    item3: "Banana",
+};
+for (var item in myObject) {
+    console.log(myObject[item]);
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+let myObject = {
+    item1: "Apple",
+    item2: "Orange",
+    item3: "Banana",
+}
+for(let item in myObject){
+    console.log(item,myObject[item])
+
+}
